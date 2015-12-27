120e797609374657f26aa9d6b6efc110b134cde4
Jaime Ramírez <mr.ljaime@gmail.com>
2015-12-26 21:41:42 -0600

    Nuevo filtro para twig.. Un pequeño Carbon!

---
+++ src/IndexBundle/Twig/AppExtension.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaime
+ * Date: 26/12/15
+ * Time: 08:36 PM
+ */
+
+namespace IndexBundle\Twig;
+
+
+class AppExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('diff', array($this, 'diffDate')),
+        );
+    }
+
+    public function diffDate($date)
+    {
+        $now = new \DateTime();
+
+        $diff = $now->diff($date);
+
+        $dias = $diff->d;
+        $horas = $diff->h;
+        $minutos = $diff->i;
+        $segundos = $diff->s;
+
+        if($dias == 0 && $horas == 0 && $minutos == 0){
+            return $diff->format("%s segundos");
+        }
+
+        if($dias == 0 && $horas == 0 && $minutos == 1){
+            return $diff->format("%i minuto");
+        }
+
+        if($dias == 0 && $horas == 0 && $minutos > 1){
+            return $diff->format("%i minutos");
+        }
+
+        if($dias == 0 && $horas == 1){
+            return $diff->format("%h hora");
+        }
+
+        if($dias == 0 && $horas > 1){
+            return $diff->format("%h horas");
+        }
+
+        if($dias == 1){
+            return $diff->format("%d día");
+        }
+
+        if($dias > 1){
+            return $diff->format("%d días");
+        }
+
+    }
+
+    public function getName()
+    {
+        return "diff_extension";
+    }
+}
