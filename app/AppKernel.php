d9c15fe85f994fb543b92b0f82382d149dc54e2c
Jaime <mr.ljaime@gmail.com>
2015-12-29 17:33:53 -0600

    Agregación de Bundle al Kernel y cambios a security

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new IndexBundle\IndexBundle(),
+            new PicboardBundle\PicboardBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
