80a01f1e0f1d3071d6b2c648452e13bee97bca8e
Jaime Ramírez <mr.ljaime@gmail.com>
2016-01-23 20:17:49 -0600

    Creación de slider para publicación

---
+++ src/IndexBundle/Entity/Slider.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace IndexBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Slider
+ *
+ * @ORM\Table(name="slider")
+ * @ORM\Entity(repositoryClass="IndexBundle\Repository\SliderRepository")
+ */
+class Slider
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\ManyToMany(targetEntity="Post")
+     * @ORM\JoinColumn(name="post_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    private $postId;
+
+    /**
+     * @var int
+     *
+     * @ORM\OneToOne(targetEntity="Picture")
+     * @ORM\JoinColumn(name="picture_id", referencedColumnName="id")
+     */
+    private $pictureId;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     */
+    private $createdAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set postId
+     *
+     * @param integer $postId
+     *
+     * @return Slider
+     */
+    public function setPostId($postId)
+    {
+        $this->postId = $postId;
+
+        return $this;
+    }
+
+    /**
+     * Get postId
+     *
+     * @return int
+     */
+    public function getPostId()
+    {
+        return $this->postId;
+    }
+
+    /**
+     * Set pictureId
+     *
+     * @param integer $pictureId
+     *
+     * @return Slider
+     */
+    public function setPictureId($pictureId)
+    {
+        $this->pictureId = $pictureId;
+
+        return $this;
+    }
+
+    /**
+     * Get pictureId
+     *
+     * @return int
+     */
+    public function getPictureId()
+    {
+        return $this->pictureId;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return Slider
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
+
