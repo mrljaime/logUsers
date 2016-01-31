8a27673366515963c8fc849d4b7ea995893fd835
Jaime Ramírez <mr.ljaime@gmail.com>
2016-01-31 00:19:12 -0600

    Venga el landing page para las publicaciones :D

--- src/IndexBundle/Controller/DefaultController.php
+++ src/IndexBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace IndexBundle\Controller;
 
+use Doctrine\Common\Proxy\Exception\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,8 +19,7 @@ class DefaultController extends Controller
             ->createQuery("select p from IndexBundle:Post p where p.isActive = true order by p.createdAt desc")
             ->setMaxResults(5)
             ->getResult();
-        $categories = $em->createQuery("select c from IndexBundle:Category c where c.isActive = true")
-        	->getResult();
+        $categories = $this->getCategories($em);
 
         return $this->render('IndexBundle:Default:home.html.twig',
             array(
@@ -27,5 +27,36 @@ class DefaultController extends Controller
                 'posts' => $result,
             	'categories' => $categories,
             ));
+    }
+
+
+    /**
+     * @Route("/post/{id}/view", name="post_view")
+     * @param Request $request
+     * @param $id
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function viewPost(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $post = $em->getRepository("IndexBundle:Post")->find($id);
+        if (!$post) {
+            throw new InvalidArgumentException("No se encontró una publicación relacinada al id");
+        }
+        $categories = $this->getCategories($em);
+        $slider = $em->createQuery("select s from IndexBundle:Slider s where s.postId = $id")
+            ->getResult();
+        return $this->render("IndexBundle:Default:viewPost.html.twig", array(
+            'categories' => $categories,
+            'post' => $post,
+            'slider' => $slider,
+        ));
+
+    }
+
+    private function getCategories($em)
+    {
+        return $em->createQuery("select c from IndexBundle:Category c where c.isActive = true")
+            ->getResult();
     }
 }
